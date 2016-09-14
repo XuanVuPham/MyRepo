79bef4717c77d113db86b13ec6e46324347ffdd1
phamxuanvu <xuanvupham96@gmail.com>
2016-09-14 13:33:42 +0700

    Changing thtwo file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def helloThuUyen
-  	render text: "Hello, Thu Uyen"  	
+  def hello
+  	render text: "Hello, wourld"  	
   end
   def helloNamPhuong
   	render text: "Hello, Nam Phuong"  	
