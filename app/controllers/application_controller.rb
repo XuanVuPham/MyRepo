60e068d60669365156f2420258fda1308b5b2b22
phamxuanvu <xuanvupham96@gmail.com>
2016-09-14 14:54:38 +0700

    fix the miss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "Hello, wourld"  	
+  	render text: "Hello, world"  	
   end
   def helloNamPhuong
   	render text: "Hello, Nam Phuong"  	
